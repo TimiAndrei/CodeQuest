1dd05c1566f5cd4606f2ffe2cced11f9b1c95c4c
alexandrazamfirescu <alexandra.zamfirescu1@s.unibuc.ro>
2025-12-02 00:51:24 +0200

    CQ-18 - Navigation & Layout
    Added navigation bar for the frontend.
    Implemented basic layout for the application.

---
+++ frontend/src/api/users.js
@@ -0,0 +1,11 @@
+import api from "./apiInstance";
+
+export const getUsers = async () => {
+  try {
+    const response = await api.get("/users/");
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    throw error;
+  }
+};
